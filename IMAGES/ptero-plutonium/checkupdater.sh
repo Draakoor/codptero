f7c1b93a8601392d5bcbf6dd70cf1763e212c1a2
Dominik <Dominique.Wellisch@t-online.de>
2024-06-03 11:12:36 +0200

    Update checkupdater.sh

--- IMAGES/ptero-plutonium/checkupdater.sh
+++ IMAGES/ptero-plutonium/checkupdater.sh
@@ -11,16 +11,14 @@ echo "64 bit system detected, downloading 64 bit updater"
 wget https://github.com/mxve/plutonium-updater.rs/releases/latest/download/plutonium-updater-x86_64-unknown-linux-gnu.tar.gz -O /home/container/updater/latestupdater.tar.gz -q --show-progress
 else
 echo "ARM system detected, downloading ARM updater"
-echo "ARM is currently not supported, please use a 64 bit system"
 exit 1
 #wget https://github.com/mxve/plutonium-updater.rs/releases/latest/download/plutonium-updater-arm64-unknown-linux-gnu.tar.gz -O /home/container/updater/latestupdater.tar.gz -q --show-progress
 fi
 #Extract the updater to /home/container/updater/ overwriting any existing files
 tar -xvf /home/container/updater/latestupdater.tar.gz -C /home/container/updater/
-#Remove the archive from /home/container/updater
-rm /home/container/updater/latestupdater.tar.gz
+#Remove the archive from /home/container/Server/updater
+rm /home/container/Server/updater/latestupdater.tar.gz
 #Run the updater
 chmod +x /home/container/updater/plutonium-updater
 echo "Updater found, running updater"
 /home/container/updater/plutonium-updater -d /home/container/Plutonium
-
