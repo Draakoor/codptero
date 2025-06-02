a8f8661952792be04d35a97422a5353f187b0315
Dominik <Dominique.Wellisch@t-online.de>
2025-06-02 17:36:35 +0200

    Update installwine.sh

--- IMAGES/ptero-wine/installwine.sh
+++ IMAGES/ptero-wine/installwine.sh
@@ -6,9 +6,9 @@ apt-get install wget gnupg2 software-properties-common -y
 
 apt install -y apt-transport-https
 
-sudo mkdir -pm755 /etc/apt/keyrings
+mkdir -pm755 /etc/apt/keyrings
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
 
 # Update repository 
 apt-get update
